b0a46cc0fb6a9a7c712cd0dba887923a7ba57435
Angus Kellett <anguskellett@hotmail.com>
2020-11-12 06:10:50 +0000

    adds remaining questions and supplies paragraph summary

--- survey.js
+++ survey.js
@@ -5,29 +5,31 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const profile = {
-
-};
+const profile = {};
 
 rl.question('What is your name? Nicknames are also acceptable :)', (answer) => {
-  console.log(`You're name: ${answer}`);
   profile.name = answer;
   rl.question('What\'s an activity you like doing?', (answer) => {
-    console.log(`You're favourite activity: ${answer}`);
-    profile.activity = answer;
+    profile.activity = answer.toLowerCase();
     rl.question('What do you listen to while doing that?', (answer) => {
-      console.log(`You like to listen to: ${answer}`);
       profile.listen = answer;
       rl.question('Which meal is your favourite? e.g. dinner, brunch, etc.', (answer) => {
-        console.log(`You're favourite meal is: ${answer}`);
-        profile.meal = answer;
-        rl.close();
-        console.log(profile);
+        profile.meal = answer.toLowerCase();
+        rl.question('What\'s your favourite thing to eat for that meal?', (answer) => {
+          profile.food = answer.toLowerCase();
+          rl.question('Which sport is your absolute favourite?', (answer) => {
+            profile.sport = answer.toLowerCase();
+            rl.question('What is your superpower?', (answer) => {
+              profile.superpower = answer.toLowerCase();
+              rl.close();
+              console.log(`Your name is ${profile.name}. You like to listen to ${profile.listen} while ${profile.activity}. You like to eat ${profile.food} for ${profile.meal}. You're favourite sport is ${profile.sport} and your superpower is ${profile.superpower}. Aren't you just fucking special.`);
+            });
+          });
+        });
       });
     });
   });
 });
-
 
 
 
